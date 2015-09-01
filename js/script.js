3f5fe8c37e02fb570ed1746fa9597c2c21aaa6cd
Syam  S Pillai <syam.sp0965@gmail.com>
2015-09-01 10:42:22 +0530

    completed all responsiveness - 3rd day starting

--- js/script.js
+++ js/script.js
@@ -1,33 +1,40 @@
+
 function show_hidden_menu() {
-  if(document.getElementById('hidden_menu').style.display == "none")
+  var menu = document.getElementById('hidden_menu');
+  var search_mob = document.getElementById('search_div');
+  var search = document.getElementById('search');
+  if(menu.style.display == "none")
   {
-    if(document.getElementById('search_div').style.display == "block")
+    if(search_mob.style.display == "block")
     {
-      document.getElementById('search_div').style.display = "none";
+      search_mob.style.display = "none";
     }
-    document.getElementById('hidden_menu').style.display = "block";
+    menu.style.display = "block";
     document.getElementById('menu_button').innerHTML = "X";
+    search.innerHTML = "<img src=\"img/search_icon.svg\" alt=\"\" width=\"28px\" height=\"28px\" />";
   }
   else {
-    document.getElementById('hidden_menu').style.display = "none";
+    menu.style.display = "none";
     document.getElementById('menu_button').innerHTML = "MENU";
   }
 }
 
 function show_search() {
-  if(document.getElementById('search_div').style.display == "none")
-  {
-    if(document.getElementById('hidden_menu').style.display == "block")
+    var search_mob = document.getElementById('search_div');
+    var menu = document.getElementById('hidden_menu');
+    var search = document.getElementById('search');
+    if(search_mob.style.display == "none")
     {
-      document.getElementById('hidden_menu').style.display = "none";
+    if(menu.style.display == "block")
+    {
+      menu.style.display = "none";
+      document.getElementById('menu_button').innerHTML = "MENU";
     }
-    document.getElementById('search_div').style.display = "block";
-  }
-  else {
-    document.getElementById('search_div').style.display = "none";
-  }
+    search_mob.style.display = "block";
+    search.innerHTML = "X";
+    }
+    else {
+    search_mob.style.display = "none";
+    search.innerHTML = "<img src=\"img/search_icon.svg\" alt=\"\" width=\"28px\" height=\"28px\" />";
+    }
 }
-
-
-
-
